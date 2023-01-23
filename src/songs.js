659b82bd77a18e5a1e70f132aecb8f36992557f6
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-01-24 00:14:58 +0100

    stage 2

--- src/songs.js
+++ src/songs.js
@@ -20,30 +20,36 @@ function orderAlphabetically(songs){
 
 //Exercise 4: Order by year, ascending
 function orderByYear(songs){
-    const Year = "1991"
     let result = songs.map(({year})=>({['year']:year}));
     let order = result.sort((a,b)=> a.year - b.year);
-    let yearSong = songs.filter(item => item.year ===  Year)
+    //let yearSong = songs.filter(item => item.year ===  order)
     console.info(result);
-    console.info(yearSong);
+    console.info(order);
     return result, order
 };
 
 //Exercise 5: Filter songs by genre
-function songsByGenre(songs) {
-    let result = songs.filter(item => item.genre.some["rock"]);
+function songsByGenre(songs, genre) {
+    let result = songs.filter(song => song.genre.includes(genre) );
     console.info(result)
     return result 
 };
 
 //Exercise 6: Modify the duration of songs to seconds
-function minutsToSeconds() {
-    //Write your code here
+function minutsToSeconds(songs) {
+    let durationseg = songs.map(({duration})=>duration);
+    
+    console.info(durationseg)
+    return durationseg
+    
 };
 
 //Exercise 7: Get the longest song
-function getLongestSong(){
-    //Write your code here
+function getLongestSong(songs){
+    let result = songs.map(({duration}) => duration);
+    //let duration = result.filter(song => song.duration === duration)
+   console.info(result)
+    return result 
 };
 
 //Exercise 8: Get the shortest song
