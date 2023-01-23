9d3bbc9f70ed5b71751cec431379851c481a00b8
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-01-23 15:30:16 +0100

    firstStage

--- src/songs.js
+++ src/songs.js
@@ -1,28 +1,39 @@
 //Exercise 1: Get the array of all Artists.
-function getAllArtists(array){
-    let result = '???';
-    //console.log("Exercise 1 ->", result);
+function getAllArtists(songs){
+    let result = songs.map(({artist})=>artist)
     return result;
 };
 
 //Exercise 2: Get the songs of a certain artist
-function getSongsFromArtist(array, artist){
-    //Write your code here
+function getSongsFromArtist(songs, artist){
+    let result = songs.filter(item =>item.artist === artist);
+    return result
 };
 
+
+
 //Exercise 3: Alphabetic order by title
-function orderAlphabetically(){
-    //Write your code here
+function orderAlphabetically(songs){
+    let result = songs.map(({title})=>title).sort();
+    return result.slice(0,10)
 };
 
 //Exercise 4: Order by year, ascending
-function orderByYear(){
-    //Write your code here
+function orderByYear(songs){
+    const Year = "1991"
+    let result = songs.map(({year})=>({['year']:year}));
+    let order = result.sort((a,b)=> a.year - b.year);
+    let yearSong = songs.filter(item => item.year ===  Year)
+    console.info(result);
+    console.info(yearSong);
+    return result, order
 };
 
 //Exercise 5: Filter songs by genre
-function songsByGenre() {
-    //Write your code here
+function songsByGenre(songs) {
+    let result = songs.filter(item => item.genre.some["rock"]);
+    console.info(result)
+    return result 
 };
 
 //Exercise 6: Modify the duration of songs to seconds
