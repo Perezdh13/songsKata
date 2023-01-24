04b0ac87a34a73084431be3576c07c81a9bc9eb1
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-01-24 16:34:34 +0100

    stage 3

--- src/songs.js
+++ src/songs.js
@@ -20,36 +20,41 @@ function orderAlphabetically(songs){
 
 //Exercise 4: Order by year, ascending
 function orderByYear(songs){
-    let result = songs.map(({year})=>({['year']:year}));
-    let order = result.sort((a,b)=> a.year - b.year);
-    //let yearSong = songs.filter(item => item.year ===  order)
-    console.info(result);
-    console.info(order);
-    return result, order
+    let order = songs.sort((a,b)=> a.year === b.year ?  (a.title < b.title ? -1 : 1 ) : a.year - b.year) 
+  return [...order]
 };
 
 //Exercise 5: Filter songs by genre
 function songsByGenre(songs, genre) {
     let result = songs.filter(song => song.genre.includes(genre) );
-    console.info(result)
-    return result 
+   // console.info(result)
+    return [...result] 
 };
 
 //Exercise 6: Modify the duration of songs to seconds
 function minutsToSeconds(songs) {
     let durationseg = songs.map(({duration})=>duration);
+    let durationSplit = durationseg + ""
+    let durationArr = durationSplit.split(" ");
+    let seconds = 0
+    let durationInSeconds = durationArr.map((time) => { let number = parseInt(time)
+        time.includes("min") ? seconds += (number*60) : seconds += number;
+   });
     
-    console.info(durationseg)
-    return durationseg
+     return [{duration:seconds}]
     
 };
+ //minutsToSeconds([{ duration: '3min 30sec' }])
+
+
 
 //Exercise 7: Get the longest song
 function getLongestSong(songs){
-    let result = songs.map(({duration}) => duration);
+    let result = songs.sort((a,b)=> b.duration - a.duration )
+    //let result = songs.map(({duration}) => duration);
     //let duration = result.filter(song => song.duration === duration)
    console.info(result)
-    return result 
+    return [{result}[0]] 
 };
 
 //Exercise 8: Get the shortest song
